12099ef8d6bff10df91a54994b2d1c79996ac1c9
Yifan Shi <stevenls1118@msn.com>
2022-10-29 19:52:57 -0600

    add /hello to server

--- express_server.js
+++ express_server.js
@@ -7,14 +7,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}.`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
 });
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
+
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
+
